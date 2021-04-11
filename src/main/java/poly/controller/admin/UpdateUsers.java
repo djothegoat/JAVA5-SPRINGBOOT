8f8fce887b0719dbf460e313483779d09eea8fd5
1Gucci <35658222+a31710@users.noreply.github.com>
2021-04-11 21:35:35 +0700

    Users
    done

--- src/main/java/poly/controller/admin/UpdateUsers.java
+++ src/main/java/poly/controller/admin/UpdateUsers.java
@@ -1,8 +1,32 @@
 package poly.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import poly.entity.Users;
+import poly.service.UserService;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/admin/")
 public class UpdateUsers {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    UserService userService;
 
+    @GetMapping("/users/update/{id}")
+    public String update(ModelMap model, @PathVariable(name = "id") Integer id){
+        Optional<Users> users = userService.findById(id);
+        model.addAttribute("users",users.get());
+        return "admin/users/update";
+    }
+
+    @PostMapping("/users/update")
+    public String postUpdate(ModelMap model, Users users){
+        userService.save(users);
+        model.addAttribute("users",new Users());
+        return "redirect:/admin/users/list";
+    }
 }
