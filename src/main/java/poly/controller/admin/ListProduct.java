8e7d408641f294cdafcbaf0f91f14e6b05e4a479
1Gucci <35658222+a31710@users.noreply.github.com>
2021-04-14 19:31:39 +0700

    Fragments
    done

--- src/main/java/poly/controller/admin/ListProduct.java
+++ src/main/java/poly/controller/admin/ListProduct.java
@@ -1,9 +1,11 @@
 package poly.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,7 @@ import poly.entity.Product;
 import poly.service.CategoryService;
 import poly.service.ProductService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -25,11 +28,48 @@ public class ListProduct {
     CategoryService categoryService;
 
     @GetMapping("product/list")
-    public String list(ModelMap model, @RequestParam(value = "name", defaultValue = "")String name){
-        List<Product> product = productService.findByName(name);
-        model.addAttribute("product",product);
-        return "admin/product/list";
+    public String list(HttpServletRequest request){
+        request.getSession().setAttribute("productlist",null);
+        return "redirect:/admin/product/page/1";
     }
+    @GetMapping("/product/page/{pageNumber}")
+    public String showProductPage(HttpServletRequest request, @PathVariable int pageNumber,
+                                  ModelMap model, @RequestParam(value = "name",defaultValue = "")String name){
+        PagedListHolder<?> pages = (PagedListHolder<?>) request.getSession().getAttribute("productlist");
+        int pagesize = 9;
+        List<Product> list = productService.findByName(name);
+        if(pages == null){
+            pages = new PagedListHolder<>(list);
+            pages.setPageSize(pagesize);
+        }else{
+            pages = new PagedListHolder<>(list);
+            final int goToPage = pageNumber - 1;
+            if(goToPage <= pages.getPageCount() && goToPage >= 0){
+                pages.setPageSize(pagesize);
+                pages.setPage(goToPage);
+            }
+        }
+        request.getSession().setAttribute("productlist",pages);
+        int current = pages.getPage() + 1;
+        int begin = Math.max(1,current-list.size());
+        int end = Math.min(begin + 20, pages.getPageCount());
+        int totalPageCount = pages.getPageCount();
 
+        System.out.println(totalPageCount);
+        String baseUrl = "/admin/product/page/";
+        model.addAttribute("beginIndex",begin);
+        model.addAttribute("endIndex",end);
+        model.addAttribute("currentIndex",current);
+        model.addAttribute("totalPageCount",totalPageCount);
+        model.addAttribute("baseUrl",baseUrl);
+        model.addAttribute("category",categoryService.findAll());
+
+        model.addAttribute("product",pages);
+        System.out.println("bat dau: " +begin);
+        System.out.println("ket thuc: "+ end);
+        System.out.println("trang hien tai: " +current);
+        System.out.println("tong trang: " +totalPageCount);
+        return "/admin/product/list";
+    }
 
 }
